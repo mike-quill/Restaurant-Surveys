e527c3151646903db985c20a02e86c01bc150bf6
Mike <mike.quill@gmail.com>
2021-12-14 16:41:57 -0500

    first component example

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <div>
+        <h1 className='text-center'>Gourmet Survey</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Header />
         </div>
     );
 }
